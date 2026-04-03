9692d8a5fea18d2c2e745a7ab2ae4812dd801e9e
Anurag Mitra <anuragmitra@Keross.local>
2026-04-03 13:29:52 +0530

    updated terms scroll

--- src/pages/TermsOfUse.jsx
+++ src/pages/TermsOfUse.jsx
@@ -16,7 +16,10 @@ export default function TermsOfUse() {
 
   return (
     <AuthLayout>
-      <div className="flex flex-col h-full min-h-[calc(100vh-8rem)]">
+      {/* CHANGED: Replaced h-full min-h-[...] with a strict h-[calc(100vh-8rem)].
+        This locks the container to the viewport, forcing the middle section to scroll.
+      */}
+      <div className="flex flex-col h-[calc(100vh-8rem)]">
         <h1 className="text-3xl font-bold mb-6 text-gray-900 shrink-0">
           Terms of Use
         </h1>
